779612af7f2908c9ec15bc1d8dbc8e296e03c7f1
syfulsharif <sharif.syful@gmail.com>
2023-07-08 20:18:07 +0600

    Input value catching function added and implemented

--- lib/screens/productCreateScreen.dart
+++ lib/screens/productCreateScreen.dart
@@ -9,6 +9,21 @@ class ProductCreateScreen extends StatefulWidget {
 }
 
 class _ProductCreateScreenState extends State<ProductCreateScreen> {
+  Map<String, String> formValues = {
+    "Img": "",
+    "ProductCode": "",
+    "ProductName": "",
+    "Qty": "",
+    "TotalPrice": "",
+    "UnitPrice": ""
+  };
+
+  inputOnChange(key, value) {
+    setState(() {
+      formValues.update(key, (value) => value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,27 +40,37 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
               child: Column(
                 children: [
                   TextFormField(
-                    onChanged: (value) {},
+                    onChanged: (value) {
+                      inputOnChange('ProductName', value);
+                    },
                     decoration: appInputDecoration('Product Name'),
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
-                    onChanged: (value) {},
+                    onChanged: (value) {
+                      inputOnChange('ProductCode', value);
+                    },
                     decoration: appInputDecoration('Product Code'),
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
-                    onChanged: (value) {},
+                    onChanged: (value) {
+                      inputOnChange('Img', value);
+                    },
                     decoration: appInputDecoration('Product Image'),
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
-                    onChanged: (value) {},
+                    onChanged: (value) {
+                      inputOnChange('UnitPrice', value);
+                    },
                     decoration: appInputDecoration('Product Unit Price'),
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
-                    onChanged: (value) {},
+                    onChanged: (value) {
+                      inputOnChange('TotalPrice', value);
+                    },
                     decoration: appInputDecoration('Product Total Price'),
                   ),
                   const SizedBox(height: 20),
@@ -62,7 +87,9 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
                         ],
                         isExpanded: true,
                         underline: Container(),
-                        onChanged: (value) {}),
+                        onChanged: (value) {
+                          inputOnChange('Qty', value);
+                        }),
                   ),
                   const SizedBox(height: 20),
                   Container(
